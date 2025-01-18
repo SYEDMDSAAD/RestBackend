bea80334b65760a6d5c7dafcd2488df81802d213
SYEDMDSAAD <134770714+SYEDMDSAAD@users.noreply.github.com>
2025-01-18 18:53:50 +0530

    hj

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-  req.setTimeout(120000); // 120 seconds
+  req.setTimeout(180000); // 120 seconds
   next();
 });
 
