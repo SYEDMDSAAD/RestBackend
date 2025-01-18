649f08857bc8b8537124bdf1e659d865de0e44b7
SYEDMDSAAD <134770714+SYEDMDSAAD@users.noreply.github.com>
2025-01-18 14:09:09 +0530

    New

--- server.js
+++ server.js
@@ -3,3 +3,5 @@ import app from "./app.js";
 app.listen(process.env.PORT, ()=>{
     console.log(`Server Running On Port ${process.env.PORT}`);
 });
+
+app.get("/", (req, res) => res.send("App is Running"));
